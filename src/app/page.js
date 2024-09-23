5b536d575bbb1e0479cce5760784a61437f5094a
RinandiArgo <rinandiargo@gmail.com>
2024-09-23 22:01:20 +0700

    Tes

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <>
+    <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+      <img
+        className="object-cover object-center w-full h-96"
+        src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"
+        alt="nature image"
+      />
+    
+       <h2 className=" text-3xl w-full">Hello, My name is</h2>
+       <h2 className=" text-3xl w-full text-slate-700">Argo</h2>
+            
+            <p className="text-center margin-0 mx-auto w-2/3	">
+                Test 123457 
+            </p>
+            <div>
+              <button className="mt-20 rounded-lg bg-red-500 w-48 min-h-12 text-white"> afadsgadgda</button>
+            </div>
+            </div>
+    </>
+  );
+}
